21c5c979294fa40fcd666995ff6e7f6df38098b5
Raphael <raphaelmicucci@gmail.com>
2024-12-06 15:34:49 -0300

    working, maybe?

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+
+// Configuração do proxy para redirecionar chamadas para a API
+app.use(
+  "/api", // Rota base para o proxy
+  createProxyMiddleware({
+    target: "http://15.228.245.31:8080", // Substitua pela URL da API
+    changeOrigin: true, // Permite alterar o cabeçalho "Host"
+  })
+);
+
+// Porta em que o servidor proxy será executado
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Proxy rodando em http://localhost:${PORT}`);
+});
